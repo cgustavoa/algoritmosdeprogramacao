65ba58e3a7a37262f9d47615939d15dd2da29bc6
cgustavoa <142316756+cgustavoa@users.noreply.github.com>
2023-09-19 10:58:57 -0300

    Add files via upload

---
+++ medias_com_matriz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+// matriz representando 2 alunos, cada um com 4 notas
+// 2 linhas, 4 colunas    
+    double notas[2][4];
+/*Representacao da matriz
+    1° aluno [][][][]
+    2° aluno [][][][]
+*/
+
+    for(int linha = 0; linha <= 1; linha ++){
+        printf("Insira as 4 notas do launo: %d\n", linha + 1);
+        for(int coluna){}
+    }
+// INCOMPLETO
+
+    notas[0][0] = 9,8
+
+    return 0;
+}
