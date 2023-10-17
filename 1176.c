585800364ea2cfcc0b9d8f03106e74d161d75aee
cgustavoa <142316756+cgustavoa@users.noreply.github.com>
2023-10-17 09:10:09 -0300

    Add files via upload

--- 1176.c
+++ 1176.c
@@ -1,23 +1,38 @@
 #include <stdio.h>
 
-int main() {
-	unsigned long long int fib[61];
-	for(int n = 0; n <= 60;n++){
-		if (n == 0) {
-			fib[0] = 0;
-		} else if (n == 1) {
-			fib[1] = 1;	
-		} else {
-			fib[n] = fib[n - 1] + fib[n - 2];
-		}
-	}
 
-	int d, v;
-	scanf("%d", &d);
-	for (int i = 0; i < d; i++) {
-			scanf("%d", &v);
-			printf("Fib(%d) = %lld\n", v, fib[v]);
-	}
+unsigned long long int fibonnaci(int n, unsigned long long int* fibo){
 
-	return 0;
+    if(fibo[n] != 0){
+        return fibo[n];
+    }
+    else if(n == 0){
+        fibo[0] = 0;
+        return 0;
+    }
+    else if(n == 1){
+        fibo[1] = 1;
+        return 1;
+    } else {
+        fibo[n] = fibonnaci(n-1, fibo) + fibonnaci(n-2, fibo);
+        return fibo[n];
+    }
+}
+
+int main(){
+    unsigned long long int fibo[61];
+
+    for(int i = 0; i <= 60;i++){
+        fibo[i] = 0;
+    }
+
+    int numTestes, posFibo;
+    scanf("%d", &numTestes);
+
+    for(int i = 0;i < numTestes;i++){
+        scanf("%d", &posFibo);
+        printf("Fib(%d) = %lld\n", posFibo, fibonnaci(posFibo, fibo));
+    }
+
+    return 0;
 }
